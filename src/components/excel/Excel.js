3f6acfbd9af07bb419d839c00ec0279b88e1b2af
Zharko Natalia <zharkonat@mail.ru>
2022-01-10 14:57:59 +0200

    add js classes

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,24 @@
+export class Excel {
+   constructor (selector, options) {
+       this.$el = document.querySelector(selector)
+       this.components = options.components || []   
+   }
+
+   getRoot () {
+        const $root = document.createElement('div')
+        $root.classList.add('excel')
+                
+        this.components.forEach( Component => {
+            const $el = document.createElement('div')
+            $el.classList.add(Component.className)
+            const component = new Component($el)
+            $el.innerHTML = component.toHTML()
+            $root.append($el)
+        })
+        return $root
+   }
+
+   render () {
+       this.$el.append(this.getRoot())
+   }
+}
