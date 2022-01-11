6527f2e2dd6302b2edc4e38c22e701db8bdde66c
Zharko Natalia <zharkonat@mail.ru>
2022-01-11 14:38:42 +0200

    add view components

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,18 +1,17 @@
+import { $ } from "../../core/dom"
+
 export class Excel {
    constructor (selector, options) {
-       this.$el = document.querySelector(selector)
+       this.$el = $(selector)
        this.components = options.components || []   
    }
 
    getRoot () {
-        const $root = document.createElement('div')
-        $root.classList.add('excel')
-                
+        const $root = $.create('div', 'excel')//document.createElement('div') + $root.classList.add('excel')      
         this.components.forEach( Component => {
-            const $el = document.createElement('div')
-            $el.classList.add(Component.className)
+            const $el = $.create('div', Component.className)
             const component = new Component($el)
-            $el.innerHTML = component.toHTML()
+            $el.html(component.toHTML())
             $root.append($el)
         })
         return $root
